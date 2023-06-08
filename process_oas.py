8ae4b0420f17099427278dd99f93f48dde7d7fec
Sai Pooja Mahajan <mahajs17@gene.com>
2023-06-08 12:15:51 -0700

    changed name

--- process_oas.py
+++ process_oas.py
@@ -5,6 +5,10 @@ import pandas as pd
 import s3fs
 from multiprocessing import Pool
 import gzip
+from pyarrow import Table, dataset
+from pyarrow.parquet import ParquetDataset
+from pyarrow.util import guid
+
 
 HEAVY_ID_COL = 'sequence_id_heavy'
 LIGHT_ID_COL = 'sequence_id_light'
@@ -53,9 +57,18 @@ def csvs_to_fastas(csv_path, filter_by=None):
         #meta_dict = _read_metadata(filter_row)
         for key, value in filter_by.items():
             if key in meta_dict:
-                if meta_dict[key].find(value)==-1:
-                    #skip this file
-                    return None
+                if isinstance(value,str): 
+                	if meta_dict[key].find(value)==-1:
+                    		#skip this file
+                            return None
+                elif isinstance(value,list):
+                    found=False
+                    for v in value:
+                     found = found or (meta_dict[key].find(v)!=-1)
+                    if not found: 
+                     return None
+                else:
+                     sys.exit('Not implemented: ', value)
     
     df = pd.read_csv(csv_path, skiprows=[0], usecols=OAS_columns, low_memory=True)
     print(len(df[OAS_columns[0]]))
@@ -112,7 +125,6 @@ def linclust_to_df(linclust_fasta, s3_path, out_path):
     fasta_lines = open(linclust_fasta).readlines()
     seq_ids = [line.rstrip()[1:] for line in fasta_lines
                     if line.startswith('>')]
-    #TODO
     file_to_heavy_map, file_to_light_map = {}, {}
     for seq_id in seq_ids:
         split_id = _split_combined_id(seq_id)
@@ -134,32 +146,110 @@ def linclust_to_df(linclust_fasta, s3_path, out_path):
         csv_file = f"s3://{file}"
         file_id = os.path.basename(csv_file).split('_')[0]
         if file_id in file_to_heavy_map:
+            meta_data = ','.join(pd.read_csv(csv_file, nrows=0).columns)
+            meta_data = json.loads(meta_data)
             df = pd.read_csv(csv_file, skiprows=[0], low_memory=True)
             df_filter =  df[df[HEAVY_ID_COL].isin(file_to_heavy_map[file_id]) &
                                 df[LIGHT_ID_COL].isin(file_to_light_map[file_id])]
+            df_filter['Species'] = [meta_data['Species'] for _ in df_filter[HEAVY_ID_COL]]
+            df_filter['Disease'] = [meta_data['Disease'] for _ in df_filter[HEAVY_ID_COL]]
+            df_filter['Vaccine'] = [meta_data['Vaccine'] for _ in df_filter[HEAVY_ID_COL]]
             outfile = os.path.basename(file).split('.')[0] + '_filtered.csv.gz'
             df_filter.to_csv(path_or_buf = f'{out_path}/{outfile}', compression="gzip", index = None, sep = ",",
                                  header=True, encoding='utf-8-sig')
-            
 
+#adapted from walk-jump-sampler code
+SALT_DEFAULT = guid()
+def pandas_to_parquet(
+    df: pd.DataFrame,
+    path: str,
+    existing_data_behavior: str = "delete_matching",
+    partition_cols=None,
+    salt: str = SALT_DEFAULT,
+):
+    """Write the dataframe as parquet to the specified path.
+
+    The data will be split into multiple files, each one containing at most 65k rows.
+    """
+    table = Table.from_pandas(df)
+
+    partitioning = None
+    if partition_cols:
+        part_schema = table.select(partition_cols).schema
+        partitioning = dataset.partitioning(part_schema, flavor="hive")
+
+    basename_template = salt + "-{i}.parquet"
+
+    parquet_format = dataset.ParquetFileFormat()
+
+    dataset.write_dataset(
+        table,
+        path,
+        partitioning=partitioning,
+        use_threads=True,
+        format=parquet_format,
+        basename_template=basename_template,
+        existing_data_behavior=existing_data_behavior,
+        max_rows_per_file=1 << 16,
+        max_rows_per_group=1 << 16,
+    )
+
+
+def parquet_to_pandas(path: str, columns=None, **kwargs) -> pd.DataFrame:
+    return (
+        ParquetDataset(path, use_legacy_dataset=False, **kwargs)
+        .read(columns=columns)
+        .to_pandas(self_destruct=True)
+    )
+
+def annotate_df_by_file_and_seq_ids(df_path, s3_path, out_path):
+    print('Untested')
+    df = parquet_to_pandas(df_path)
+    file_id = os.path.basename(df_path).split('_')[0]
+    print(file_id)
+    s3 = s3fs.S3FileSystem()
+    files = s3.glob(s3_path + f'/{file_id}*')
+    print(s3_path + f'/{file_id}_*')
+    #print(files)
+    assert len(files) == 1
+    s3_file = files[0]
+    meta_dict = ','.join(pd.read_csv(s3_file, nrows=0).columns)
+    meta_dict = json.loads(meta_dict)
+    df['Species'] = meta_dict['Species']
+    os.makedirs(out_path, exist_ok=True)
+    outfile = out_path + '/' +os.path.basename(df_path).split('.')[0] + '_with_species.csv'
+    #df.to_csv(path_or_buf = f'{out_path}/{outfile}', compression="gzip", index = None, sep = ",",
+    #         header=True, encoding='utf-8-sig')
+    SPLIT_COLUMN = ['partition']
+    pandas_to_parquet(df, outfile, partition_cols=SPLIT_COLUMN)
 
 
 if __name__ == '__main__':
+    print('Not implemented')
     #process_files(outpath='processed_pOAS_fastas', basename='pairedseqs')
     #filters = {'Vaccine': 'None','Disease': 'None'}
     #process_files(outpath='processed_pOAS_fastas_novac_nodis', basename='pairedseqs_novac_nodis',
     #            filter_by=filters)
-    filters = {'Vaccine': 'None','Disease': 'None','Species':'human'}
-    process_files(outpath='processed_pOAS_fastas_human_novac_nodis', basename='pairedseqs_human_novac_nodis',
-                filter_by=filters)
-    filters = {'Species':'human'}
-    process_files(outpath='processed_pOAS_fastas_human', basename='pairedseqs_human',
-                filter_by=filters)
+    #filters = {'Vaccine': 'None','Disease': 'None','Species':'human'}
+    #process_files(outpath='processed_pOAS_fastas_human_novac_nodis', basename='pairedseqs_human_novac_nodis',
+    #            filter_by=filters)
+    #filters = {'Species':['human', 'rabbit']}
+    #process_files(outpath='processed_pOAS_fastas_human_rabbit', basename='pairedseqs_human_rabbit',
+    #            filter_by=filters)
+    #others 'HIV'
+    #filters = {'Disease': 'SARS-COV-2'}
     #linclust_to_df('linclust_out/pOAS_hlcombined_novac_nodis_linclust_seqid95c80_rep_seq.fasta',
     #               "s3://prescient-data-dev/sandbox/freyn6/raw/poas/",
     #               "/gstore/scratch/u/mahajs17/repositories/antibody_database/linclust_naive")
     #linclust_to_df('linclust_out/pOAS_hlcombined_linclust_seqid95c80_rep_seq.fasta',
     #               "s3://prescient-data-dev/sandbox/freyn6/raw/poas/",
     #               "/gstore/scratch/u/mahajs17/repositories/antibody_database/linclust_all")
-   
-
+    #linclust_to_df('linclust_out/pOAS_hlcombined_human_novac_nodis_linclust_seqid95c80_rep_seq.fasta',
+    #               "s3://prescient-data-dev/sandbox/freyn6/raw/poas/",
+    #               "/gstore/scratch/u/mahajs17/repositories/antibody_database/linclust_naive_human") 
+    #linclust_to_df('linclust_out/pOAS_hlcombined_human_rabbit_linclust_seqid95c80_rep_seq.fasta',
+    #               "s3://prescient-data-dev/sandbox/freyn6/raw/poas/",
+    #               "/gstore/scratch/u/mahajs17/repositories/antibody_database/linclust_naive_human_rabbit")
+    #annotate_df_by_file_and_seq_ids("s3://prescient-data-dev/sandbox/mahajs17/walk-jump/data/parquet/large_molecules/linclust_poas_naive/",
+    #                "s3://prescient-data-dev/sandbox/freyn6/raw/poas/",
+    #              "/gstore/scratch/u/mahajs17/repositories/antibody_database/linclust_naive_human_annotated")
